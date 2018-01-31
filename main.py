c1a433e2a785c2a2734081996fcb34cbcf662ca8
Theo <theo.despoudis@teckro.com>
2018-01-31 09:57:21 +0000

    Updated translations

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import gettext
 
-el = gettext.translation('base', localedir='locales', languages=['el'])
+el = gettext.translation('base', localedir='locales', languages=['el'], fallback=False)
 el.install()
 _ = el.gettext # Greek
+ngettext = el.ngettext
 
 
 def print_some_strings():
     print(_("Hello world"))
     print(_("This is a translatable string"))
 
-print_some_strings()
+def print_some_plural_strings(num):
+    message1 = ngettext('{0} Human', '{0} Humans', num)
+    message2 = ngettext('I possess {0} laptop', 'I possess {0} laptops', num)
+
+    print(message1.format(num))
+    print(message2.format(num))
+
+
+print_some_plural_strings(1)
+print_some_plural_strings(5)
